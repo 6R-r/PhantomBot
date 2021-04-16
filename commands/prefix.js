f0bd1d8ec4be53b0401549b4d31669cbfa3f6da4
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-04-16 17:36:46 -0500

    Bit

--- commands/prefix.js
+++ commands/prefix.js
@@ -32,5 +32,5 @@ module.exports.config = {
   module: 'Settings',
   description: 'Change the prefix for the bot.',
   usage: ['prefix <new prefix>'],
-  requires: [0x000000020]
+  requires: [32]
 };
