712ff5966c76cb9785044810c50b5bc730c6aa34
Zachary M <31627113+zmontgo@users.noreply.github.com>
2021-05-03 09:54:12 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -67,7 +67,7 @@ module.exports = async (client, message) => {
         guild: message.guild.id
       });
 
-      if (stats && parseInt(stats) !== NaN) {
+      if (stats && parseInt(stats.total) !== NaN) {
         total = parseInt(stats.total);
       }
 
