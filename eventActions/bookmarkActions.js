38a4ec064e6b683d49657dc4b57f5cde562476d0
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 12:05:12 -0500

    Fix embeds

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -35,7 +35,7 @@ class bookmarkActions {
         }
       });
       
-      await user.send(bookmarkMessage, {files: attachments});
+      await user.send({embed=bookmarkMessage, files: attachments});
       
       return true;
     }
