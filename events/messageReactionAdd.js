8f5a67d9f696eab463a1905ab8df3f9fcbf0319e
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 11:33:26 -0500

    Test

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,13 +3,12 @@ const bookmarkActions = require('../eventActions/bookmarkActions');
 module.exports = async (client, reaction, user) => {
   // When we receive a reaction we check if the reaction is partial or not
   if (!reaction || reaction.partial) {
-    console.log('Reaction didn\'t exist?')
+    console.log(reaction)
+    console.log(user)
     // If the message this reaction belongs to was a partial we ignore it
     // Because raw.js should run this after fetching the partial
     return;
   }
-
-  console.log(user)
   
   // Bookmark messages in DMs
   bookmarkActions.addBookmark(client, user, reaction);
