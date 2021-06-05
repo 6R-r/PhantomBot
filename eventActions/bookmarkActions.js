6774250d63f3878be89ba1f30ada7c50347789db
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 12:39:51 -0500

    Use first instance of split

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -22,7 +22,7 @@ class bookmarkActions {
         att.array().forEach((image) => {
           var image_url = path.parse(image.url);
 
-          image_url = `${image_url.dir}/${image_url.name}/${image_url.ext.split(/[^A-Za-z]/)[0]}`;
+          image_url = `${image_url.dir}/${image_url.name}/${image_url.ext.split(/[^A-Za-z]/)[1]}`;
 
           attachments.push(image_url);
         });
@@ -33,7 +33,7 @@ class bookmarkActions {
         if (embed.image) {
           var image_url = path.parse(embed.image.url);
 
-          image_url = `${image_url.dir}/${image_url.name}/${image_url.ext.split(/[^A-Za-z]/)[0]}`;
+          image_url = `${image_url.dir}/${image_url.name}/${image_url.ext.split(/[^A-Za-z]/)[1]}`;
 
           attachments.push(image_url);
         }
