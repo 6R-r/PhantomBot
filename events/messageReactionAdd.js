3afc69c400194b4c1da6d01167e3455c31319201
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 11:04:16 -0500

    Remove double reaction

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,17 +1,12 @@
 const bookmarkActions = require('../eventActions/bookmarkActions');
 
 module.exports = async (client, reaction, user) => {
-	try {
-		// When we receive a reaction we check if the reaction is partial or not
-		if (reaction.partial) {
-			// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-			reaction = await reaction.fetch();
-		}
-	} catch (error) {
-		console.error('Something went wrong when fetching the message: ', error);
-		// Return as `reaction.message.author` may be undefined/null
-		return;
-	}
+  // When we receive a reaction we check if the reaction is partial or not
+  if (reaction.partial) {
+    // If the message this reaction belongs to was a partial we ignore it
+    // Because raw.js should run this after fetching the partial
+    return;
+  }
   
   // Bookmark messages in DMs
   bookmarkActions.addBookmark(client, user, reaction);
