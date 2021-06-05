4708b4b0aa66d65202b7fb8045044b2d8ea107d2
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 10:42:42 -0500

    Fix variable

--- eventActions/bookmarkActions.js
+++ eventActions/bookmarkActions.js
@@ -21,7 +21,7 @@ class bookmarkActions {
         bookmarkMessage.setImage(att);
       }
 
-      message.embeds.forEach((embed) => {
+      reaction.message.embeds.forEach((embed) => {
         if (embed.description) bookmarkMessage.addField('Embed Description', embed.description);
         if (embed.image) bookmarkMessage.setImage(embed.image.url);
         if (embed.fields.length > 0) console.log(embed.fields);
