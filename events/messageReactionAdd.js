42ddc62a3f524f7f058323cedb1b3c704f089c87
Zachary Montgomery <31627113+zmontgo@users.noreply.github.com>
2021-06-05 11:47:15 -0500

    Remove old testing

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,8 +3,6 @@ const bookmarkActions = require('../eventActions/bookmarkActions');
 module.exports = async (client, reaction, user) => {
   // When we receive a reaction we check if the reaction is partial or not
   if (!reaction || reaction.partial) {
-    console.log(reaction)
-    console.log(user)
     // If the message this reaction belongs to was a partial we ignore it
     // Because raw.js should run this after fetching the partial
     return;
